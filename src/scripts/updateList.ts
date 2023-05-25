5fe08c0d4ac8a1741b6d9a348e4ccde6afa23152
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-05-25 09:38:27 -0700

    move functions out

--- src/scripts/updateList.ts
+++ src/scripts/updateList.ts
@@ -1,6 +1,8 @@
 import { Item, ListStore } from "../store/listStore";
 import { createElements } from "../ui-lib/createElement";
 import { updateRecord } from "./web5/web5Helpers";
+import { renderButtonItemControls } from "../ui-lib/listItemUi";
+
 
 const listStore = new ListStore('gg-list-store');
 
@@ -36,70 +38,7 @@ async function changeItemStatus ( item: Item, event: MouseEvent):Promise<void> {
 };
 
 
-const renderEditInput = (item: Item, event: MouseEvent):void  => {
-    event.stopPropagation();
-    const liContainer = document.querySelector('.grocery-list-item-container')!;
-    const liButtonContainer = liContainer.querySelector<HTMLDivElement>('.grocery-list-item-button-container')!;
-    const liEditButton = liButtonContainer.querySelector<HTMLDivElement>('.grocery-list-edit-button')!;
-    const liDeleteButton = liButtonContainer.querySelector<HTMLDivElement>('.grocery-list-delete-button')!;
-        // change text container into input with value of text container
-        liContainer.innerHTML = '';
-        const liInput = createElements({
-            type: 'input',
-            attr: [
-                { name: 'type', value: 'text' }, 
-                { name: 'value', value: item.data.body },
-                {name: 'class', value: 'search-input'}
-            ]
-        });
-        liInput.classList.add('grocery-list-item-input');
-        liContainer.appendChild(liInput);
-        const liSaveButton = createElements({
-            type: 'button',
-            attr: [{ name: 'class', value: 'grocery-list-save-button' }]
-        });
-        liSaveButton.textContent = 'S';
-        liSaveButton.classList.add('control-btn');
-        liButtonContainer.removeChild(liEditButton);
-        liButtonContainer.removeChild(liDeleteButton);
-        liButtonContainer.appendChild(liSaveButton);
-        liContainer.appendChild(liButtonContainer);
-        liInput.focus();
-};
 
-const renderButtonItemControls = (item: Item):HTMLElement => {
-    const liButtonContainer = createElements({ 
-        type: 'div', 
-        attr: [
-            { name: 'class', value: 'grocery-list-item-button-container' }
-        ] 
-    });
-    const liEditButton = createElements({ 
-        type: 'div', 
-        attr: [
-            { name: 'class', value: 'grocery-list-edit-button' }
-        ] 
-    });
-    liEditButton.textContent = 'E';
-    liEditButton.classList.add('control-btn');
-    const liDeleteButton = createElements({ 
-        type: 'div', 
-        attr: [
-            { name: 'class', value: 'grocery-list-delete-button' }
-        ] 
-    });
-
-    liEditButton.addEventListener('click', function(event: MouseEvent){
-        renderEditInput.call(this, item, event);
-    });
-    
-    liDeleteButton.textContent = 'D';
-    liDeleteButton.classList.add('control-btn');
-    liButtonContainer.appendChild(liEditButton);
-    liButtonContainer.appendChild(liDeleteButton);
-
-    return liButtonContainer;
-};
 
 export const updateList = (updatedList: Item[]) => {
 
---
+++ src/ui-lib/listItemUi.ts
@@ -0,0 +1,67 @@
+import { createElements } from './createElement';
+import { Item } from '../store/listStore';
+
+const renderEditInput = (item: Item, event: MouseEvent):void  => {
+    event.stopPropagation();
+    const liContainer = document.querySelector('.grocery-list-item-container')!;
+    const liButtonContainer = liContainer.querySelector<HTMLDivElement>('.grocery-list-item-button-container')!;
+    const liEditButton = liButtonContainer.querySelector<HTMLDivElement>('.grocery-list-edit-button')!;
+    const liDeleteButton = liButtonContainer.querySelector<HTMLDivElement>('.grocery-list-delete-button')!;
+        // change text container into input with value of text container
+        liContainer.innerHTML = '';
+        const liInput = createElements({
+            type: 'input',
+            attr: [
+                { name: 'type', value: 'text' }, 
+                { name: 'value', value: item.data.body },
+                {name: 'class', value: 'search-input'}
+            ]
+        });
+        liInput.classList.add('grocery-list-item-input');
+        liContainer.appendChild(liInput);
+        const liSaveButton = createElements({
+            type: 'button',
+            attr: [{ name: 'class', value: 'grocery-list-save-button' }]
+        });
+        liSaveButton.textContent = 'S';
+        liSaveButton.classList.add('control-btn');
+        liButtonContainer.removeChild(liEditButton);
+        liButtonContainer.removeChild(liDeleteButton);
+        liButtonContainer.appendChild(liSaveButton);
+        liContainer.appendChild(liButtonContainer);
+        liInput.focus();
+};
+
+export const renderButtonItemControls = (item: Item):HTMLElement => {
+    const liButtonContainer = createElements({ 
+        type: 'div', 
+        attr: [
+            { name: 'class', value: 'grocery-list-item-button-container' }
+        ] 
+    });
+    const liEditButton = createElements({ 
+        type: 'div', 
+        attr: [
+            { name: 'class', value: 'grocery-list-edit-button' }
+        ] 
+    });
+    liEditButton.textContent = 'E';
+    liEditButton.classList.add('control-btn');
+    const liDeleteButton = createElements({ 
+        type: 'div', 
+        attr: [
+            { name: 'class', value: 'grocery-list-delete-button' }
+        ] 
+    });
+
+    liEditButton.addEventListener('click', function(event: MouseEvent){
+        renderEditInput.call(this, item, event);
+    });
+    
+    liDeleteButton.textContent = 'D';
+    liDeleteButton.classList.add('control-btn');
+    liButtonContainer.appendChild(liEditButton);
+    liButtonContainer.appendChild(liDeleteButton);
+
+    return liButtonContainer;
+};
